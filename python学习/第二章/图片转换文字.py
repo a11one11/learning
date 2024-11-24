2cf0e9d0dd966314f0e207e3631d232c25e0bec5
a11one11 <2716250931@qq.com>
2024-11-24 17:45:01 +0800

    学习py第三天

--- python学习/第二章/图片转换文字.py
+++ python学习/第二章/图片转换文字.py
@@ -1,3 +1,7 @@
+# import easyocr
+# easyocr_reader =easyocr.Reader(['ch_sim','en'])#ch_sim，是简体中文模型
+# print(easyocr_reader.readtext("ocr.png"))
 import easyocr
 easyocr_reader =easyocr.Reader(['ch_sim','en'])
-print(easyocr_reader.readtext(ocr.png))
+result = easyocr_reader.readtext("ocr.png")
+print([one[1] for one in result])
